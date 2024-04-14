0620363c8ccc098317f9db23c28f867fb544a9e5
Benhin <benhinmwendwa7@gmail.com>
2024-04-14 21:00:27 +0300

    created a function fetchVehicleInfo and declared  variables

---
+++ src/script.js
@@ -0,0 +1,5 @@
+function fetchVehicleInfo() {
+    const vehicleType = document.getElementById('vehicleType').value;
+    const brandCode = document.getElementById('brands').value;
+    const modelCode = document.getElementById('models').value;
+    const yearCode = document.getElementById('years').value;
